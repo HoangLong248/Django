0a0247161a387902b0321b868fc124f866af23d6
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-08-13 19:47:33 +0700

    Complete module: urls and link building; templates, styling and Static Content

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Meeting, Room
 
 # Create your views here.
+def detail(request, id):
+    try:
+        meeting = Meeting.objects.get(pk=id)
+        return render(request, 'meetings/detail.html', {"meeting": meeting})
+    except Meeting.DoesNotExist:
+        raise Http404("Page not found")
+
+def room_list(request):
+    try:
+        room = Room.objects.all()
+        return render(request, 'meetings/list_room.html', {"rooms": room})
+    except Room.DoesNotExist:
+        raise Http404("Page not found")
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from meetings.models import Meeting, Room
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Hello Welcome To Webpage")
+    return render(request, 'website/welcome.html', 
+                {"meetings": Meeting.objects.all(),"rooms": Room.objects.all(), "name": "Long Tran"})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
 
-
 # Please add: An about page that shows some text about yourself
 def about(request):
-    return HttpResponse("Hello Django Project on Pluralsight. My name is John.")
+    return HttpResponse("Hello Django Project on Pluralsight. My name is John.") 
